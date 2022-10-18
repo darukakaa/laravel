685a9e79b7fde320772c8cac9c7c2e7aee17cab0
LAPTOP-QIQ4NRNI\LENOVO <darrukk01@gmail.com>
2022-10-18 14:08:42 +0700

    11

---
+++ UTS/resources/views/admin-dashboar.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <x-slot name="header">
+    Admin Dashboard
+    </x-slot>
+
+</x-app-layout>
---
+++ UTS/resources/views/user-dashboard.blade.php
@@ -0,0 +1,6 @@
+<x-app-layout>
+    <x-slot name="header">
+    User Dashboard
+    </x-slot>
+
+</x-app-layout>
